76ed2eb15794f201340c369d942055bba2b1263a
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-09-04 20:22:37 -0500

    Initial Design
    - Design setup.  Next vids and colors are needed.

---
+++ src/Home.js
@@ -0,0 +1,30 @@
+import './Home.css';
+import { Row, Col, Container} from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container fluid>
+      <Row className="mainRow">
+        
+        <Col md={5} className='section1'>
+            test
+        </Col>
+
+        <Col md={3} className='section2'>
+        test
+        </Col>
+
+        <Col md={2} className='section3'>
+        test
+        </Col>
+
+        <Col md={2} className='section4'>
+        test
+        </Col>
+
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
