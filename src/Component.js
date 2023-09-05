4faaf5860f194f0ad4855eb8295133fea42eff64
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-09-05 01:55:46 -0500

    First Avi Made
    - Greyscale and CSS Effects added.
    - Hover pause/play feature added.

---
+++ src/Component.js
@@ -0,0 +1,60 @@
+// YourComponent.js
+import React, { Component } from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
+
+import AvatarVid1WebM from './img/4444trans_avi_1.webm';
+
+class YourComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // Add event listeners to control video playback
+    this.section4 = document.querySelector('.section4'); // Replace with a more specific selector if needed
+    this.section4.addEventListener('mouseenter', this.playVideo);
+    this.section4.addEventListener('mouseleave', this.pauseVideo);
+  }
+
+  componentWillUnmount() {
+    // Remove event listeners when the component unmounts
+    this.section4.removeEventListener('mouseenter', this.playVideo);
+    this.section4.removeEventListener('mouseleave', this.pauseVideo);
+  }
+
+  playVideo = () => {
+    // Play the video when hovering
+    if (this.videoRef.current) {
+      this.videoRef.current.play();
+    }
+  };
+
+  pauseVideo = () => {
+    // Pause the video when no longer hovering
+    if (this.videoRef.current) {
+      this.videoRef.current.pause();
+    }
+  };
+
+  render() {
+    return (
+      <Col md={2} className='section4'>
+        <p className='avatarTitle'>BarryBoy</p>
+        <video
+          ref={this.videoRef}
+          loop
+          muted
+          className="AvatarVid1"
+          alt="Avatar 1 Vid"
+          poster="test.png"
+        >
+          <source src={AvatarVid1WebM} type="video/webm" />
+          {/* Add additional source elements for different video formats if needed */}
+        </video>
+      </Col>
+    );
+  }
+}
+
+export default YourComponent;
--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,27 @@
+// home.js
+import React from 'react';
 import './Home.css';
-import { Row, Col, Container} from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 
 import AvatarVid1WebM from './img/4444trans_avi_1.webm';
+
+import YourComponent from './Component.js'; // Import YourComponent
 
 function App() {
   return (
     <Container fluid>
       <Row className="mainRow">
-        
         <Col md={5} className='section1'>
-            tes
+          tes
         </Col>
-
         <Col md={3} className='section2'>
-        test
+          test
         </Col>
-
         <Col md={2} className='section3'>
-        test
+          test
         </Col>
 
-        <Col md={2} className='section4'>
-        
-       <video autoPlay loop muted className="AvatarVid1" alt="Avatar 1 Vid" poster="path_to_poster_image.jpg">
-        <source src={AvatarVid1WebM} type="video/webm" />
-        {/* Add additional source elements for different video formats if needed */}
-      </video>
-
-
-        </Col>
+        <YourComponent /> {/* Render YourComponent within App */}
 
       </Row>
     </Container>
