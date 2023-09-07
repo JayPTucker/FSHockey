00d54f277dc7b4fdf90aa7340b745dc144b442e1
Jay Paul Tucker <jaypaultucker@gmail.com>
2023-09-06 22:37:18 -0500

    Mac Changes Push

--- src/app/Section1.js
+++ src/app/Section1.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import { Row, Col, Container } from 'react-bootstrap';
-import favicon from "../img/Section1/favicon.ico"
+import { Col } from 'react-bootstrap';
+// import favicon from "../img/Section1/favicon.ico"
+import './Section1.css'
+
+import HeaderPic from '../img/Section1/headertest.png';
 
 class YourComponent3 extends Component {
     render() {
         return (
             <Col md={5} className='section1'>
-                <a className="credits" href="https://jaypaultucker.org" rel="noopener noreferrer" target="_blank">
+
+                <img src={HeaderPic} className='headerPic' alt="HeaderPic"></img>
+        
+
+                {/* <a className="credits" href="https://jaypaultucker.org" rel="noopener noreferrer" target="_blank">
                 <img className="navbar-logo" alt="My logo" src={favicon}></img>
                 <p>Copyright © Jay Paul Tucker 2023</p>
-                </a>
+                </a> */}
             </Col>
         )
     }
